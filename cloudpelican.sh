4517fed17cadc29de4979f8555b7d89db272194a
Robin Verlangen <robin+github@us2.nl>
2013-12-05 16:29:05 +0100

    Create cloudpelican.sh

---
+++ cloudpelican.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Author: Robin Verlangen
+# Very simpel logging method for bash
+# Requires: perl, wget, hostname
+
+# Config
+API_TOKEN='PUT_YOUR_API_TOKEN_HERE'
+
+# Log method
+log() {
+        # Endpoint for api calls
+        ENDPOINT='https://api.cloudpelican.com/api/push/pixel'
+
+        # Message
+        MSG=$1
+        MSG_ENC="$(perl -MURI::Escape -e 'print uri_escape($ARGV[0]);' "$MSG")"
+
+        # Hostname
+        HOSTNAME=`hostname`
+        HOSTNAME_ENC="$(perl -MURI::Escape -e 'print uri_escape($ARGV[0]);' "$HOSTNAME")"
+
+        # Full uri
+        FULL_URL="$ENDPOINT?t=$API_TOKEN&f[msg]=$MSG_ENC&f[host]=$HOSTNAME_ENC"
+        echo $FULL_URL
+
+        # Post data
+        wget -o /dev/null -nv --timeout=3 $FULL_URL
+}
+
+# Test command
+log "Hello CloudPelican from bash"
